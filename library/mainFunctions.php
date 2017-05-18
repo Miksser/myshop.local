53fae9b0777010541c9c78d1dcf9d67b4ed1c42b
Maksim Serdziukou <onemikser@gmail.com>
2017-05-18 22:03:50 +0300

    1.4.4

--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -22,3 +22,19 @@ function loadTemplate($smarty, $templateName)
 {
     $smarty->display($templateName . TemplatePostfix);
 }
+
+/**
+ * Дебаггер, останавливает сайт при die = 1
+ * @param null $value переменная которую надо проверить
+ * @param int $die остановить работу сайта 1 да 0 нет
+ */
+function d($value = null, $die = 1)
+
+{
+
+    echo  'Debug: </br></pre>';
+    print_r($value);
+    echo '</pre>';
+
+    if($die) die;
+}
