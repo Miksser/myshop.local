45c3a2a8d23f4b05dc9822a8fb676db9157be133
Maxim Serdjukov <onemikser@gmail.com>
2017-05-24 16:55:52 +0300

    2.3

--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -6,7 +6,8 @@
  *
  */
 
-function loadPage ($smarty, $controlleName, $actionName = 'index') {
+function loadPage($smarty, $controlleName, $actionName = 'index')
+{
     include_once PathPrefix . $controlleName . PathPostfix;
 
     $function = $actionName . 'Action';
@@ -32,9 +33,28 @@ function d($value = null, $die = 1)
 
 {
 
-    echo  'Debug: </br></pre>';
+    echo 'Debug: </br></pre>';
     print_r($value);
     echo '</pre>';
 
-    if($die) die;
+    if ($die) die;
+}
+
+/**
+ * Преобразование результата работы функции выборки в ассоциативный массив
+ *
+ * @param $rs набор строк - результат работы Select
+ * @return array|bool
+ */
+
+
+function createSmartyRsArray($rs)
+{
+    if (!$rs) return false;
+
+    $smartyRs = [];
+    while ($row = mysql_fetch_assoc($rs)) {
+        $smartyRs[] = $row;
+    }
+    return $smartyRs;
 }
--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -3,6 +3,26 @@
  * Модель работает только с категориями товаров. (планшеты, телефоны)
  */
 
+/**
+ * Получить дочернии категории для категория $catID
+ *
+ * @param $catId ID категории
+ * @return mixed массив дочерних категорий
+ */
+
+function getChildrenForCat($catId)
+{
+    $sql = "Select * FROM categories  WHERE parent_id = '{$catId}'";
+
+    $rs = mysql_query($sql);
+
+    return createSmartyRsArray($rs);
+}
+
+/**
+ * Получить главные категории с приявзками дочерних
+ * @return array массив категорий
+ */
 function getAllMainCatsWithChildren()
 {
 
@@ -11,7 +31,15 @@ function getAllMainCatsWithChildren()
     $rs = mysql_query($sql);
     $smartyRs = array();
     while ($row = mysql_fetch_assoc($rs)) {
+
+        $rsChildren = getChildrenForCat($row['id']);
+
+        if ($rsChildren) {
+            $row['children'] = $rsChildren;
+        }
+
         $smartyRs[] = $row;
+
     }
 
     return $smartyRs;
