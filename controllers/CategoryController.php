3ff5eb22ed6b0a86ac5d9b87739ea6f4f8676425
Maxim Serdjukov <onemikser@gmail.com>
2017-05-29 13:01:31 +0300

    3.2.1

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -13,6 +13,7 @@ include_once '../models/ProductsModel.php';
  * Формирование страницы категорий товаров
  * @param $smarty
  */
+
 function indexAction($smarty)
 {
     $catId = isset($_GET['id']) ? $_GET['id'] : null;
