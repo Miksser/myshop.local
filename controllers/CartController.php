9987c0adac351744e7bc7d01a612aa877a138b83
Maxim Serdjukov <onemikser@gmail.com>
2017-05-31 15:46:10 +0300

    3.5

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -13,5 +13,17 @@ include_once '../models/ProductsModel.php';
 
 function addtocartAction()
 {
+    $itemId = isset($_GET['id'] ? intval($_GET['id']) : null);
+    if (!$itemId) return false;
 
+    $resData = [];
+    if (isset($_SESSION['cart']) && array_search($itemId, $_SESSION['cart'] === fasle)) {
+        $_SESSION['cart'][] = $itemId;
+        $resData['cntItems'] = count($_SESSION['cart']);
+        $resData['success'] = 1;
+        } else {
+        $resData['success'] = 0;
+    };
+
+    echo json_encode($resData);
 }
--- www/js/main.js
+++ www/js/main.js
@@ -5,5 +5,18 @@
 
 
 function addToCart(itemId) {
-
+    console.log('js- addToCart()');
+    $.ajax({
+        type: 'POST',
+        async: false,
+        url: "/cart/addtocart/" + itemId + "/",
+        dataType: 'json',
+        success: function (data) {
+            if (data['success']) {
+                $('#cartCtnItems').html(data['ctnItems']);
+                $('#addCart_' + itemId).hide();
+                $('#removeCart_' + itemId).show();
+            }
+        }
+    })
 }
